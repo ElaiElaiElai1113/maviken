fab8d1b14af8e1cd10bcf03e29768fd3538c2ce0
ElaiElaiElai1113 <115462893+ElaiElaiElai1113@users.noreply.github.com>
2024-02-24 23:50:28 +0800

    Finished login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:maviken/screens/loginscreen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -13,7 +9,8 @@ Future<void> main() async {
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlud2VkbWh6aGphZW5zdWF3Y3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg3Njg1ODUsImV4cCI6MjAyNDM0NDU4NX0.E0ErllIEXy9sCA3ynUw2Ta7XJIZXj6MXRtJSArFoXbo',
   );
-  runApp(MyApp());
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,10 +18,153 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Maviken',
-      home: LoginScreen(),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Maviken',
+        home: LoginScreen(),
+        routes: {
+          HomeScreen.routeName: (context) => const HomeScreen(),
+        });
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = '/HomeScreen';
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  final supabase = Supabase.instance.client;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: const Color(0xFFdcd8d7),
+        child: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height * .5,
+            width: MediaQuery.of(context).size.width * .4,
+            decoration: const BoxDecoration(
+              color: Color(0xFF0a438f),
+              borderRadius: BorderRadius.all(
+                Radius.circular(15),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  child: Image(
+                    image: AssetImage('lib/assets/mavikenlogo.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 70),
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFeab557),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                      prefixIconColor: Colors.white,
+                      labelText: 'Username',
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 70),
+                  child: TextField(
+                    obscureText: true,
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFeab557),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                      prefixIconColor: Colors.white,
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width * .2,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      final email = _emailController.text;
+                      final password = _passwordController.text;
+
+                      final response = await supabase.auth
+                          .signInWithPassword(email: email, password: password);
+
+                      if (response.user != null) {
+                        Navigator.pushReplacementNamed(
+                            context, HomeScreen.routeName);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text('Invalid email or password')),
+                        );
+                      }
+                    },
+                    style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                        Color(0xFFeab557),
+                      ),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 }
--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class second extends StatelessWidget {
+  const second({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height * .5,
-          width: MediaQuery.of(context).size.width * .42,
-          color: Color(0xFF0a438f),
-        ),
-      ),
+      body: Text('Hello'),
     );
   }
 }
